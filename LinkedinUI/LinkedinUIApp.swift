25ca1c70d3b21736039e3799678c6182cf72b5f8
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-10 15:52:59 +0530

    Initial Commit

---
+++ LinkedinUI/LinkedinUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinUIApp.swift
+//  LinkedinUI
+//
+//  Created by Lucas Pereira on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
